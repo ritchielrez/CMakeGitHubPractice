10f8a1df1a50339608bda57386389d8ddfb70509
ritch <ritchielrez@gmail.com>
2020-09-23 12:12:13 +0600

    Updated test.h file

--- include/test.h
+++ include/test.h
@@ -4,3 +4,18 @@ inline int add(int num1, int num2)
 {
     return num1 + num2;
 }
+
+inline int sub(int num1, int num2)
+{
+	return num1 - num2;
+}
+
+inline int mul(int num1, int num2)
+{
+	return num1 * num2;
+}
+
+inline float div(int num1, int num2)
+{
+	return (float)num1 / (float)num1;
+}
